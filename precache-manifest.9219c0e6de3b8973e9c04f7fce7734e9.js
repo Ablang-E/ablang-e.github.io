c7a0cc3f6ad3be8cbe48fda43f674461f8cab5e6
Eric Ablang <eablan2@uic.edu>
2020-08-18 14:50:44 -0500

    Updates

---
+++ precache-manifest.9219c0e6de3b8973e9c04f7fce7734e9.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f61df229e303bb98cbb33a956eb9c5ed",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "36e20f1c5be58e22fbc7",
+    "url": "/portfolio/static/css/2.a6bc1c31.chunk.css"
+  },
+  {
+    "revision": "801e2d4aba30769ba83e",
+    "url": "/portfolio/static/css/main.39b005a0.chunk.css"
+  },
+  {
+    "revision": "36e20f1c5be58e22fbc7",
+    "url": "/portfolio/static/js/2.9428b9e8.chunk.js"
+  },
+  {
+    "revision": "cf7302ab57415842ba2bf20f719dee93",
+    "url": "/portfolio/static/js/2.9428b9e8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "801e2d4aba30769ba83e",
+    "url": "/portfolio/static/js/main.b2ffdebd.chunk.js"
+  },
+  {
+    "revision": "0d98471f6c3b69ca4251",
+    "url": "/portfolio/static/js/runtime-main.44087e9b.js"
+  },
+  {
+    "revision": "fadabf86a9f27d69dab7afb43b67a94c",
+    "url": "/portfolio/static/media/UIC_CS_logo.fadabf86.png"
+  },
+  {
+    "revision": "4ff367022653ed532ce1f387586b9a06",
+    "url": "/portfolio/static/media/front-img.4ff36702.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/landingpage/precache-manifest.96f7aaaadf9fcd7da211a9785392ac65.js"
+  "/portfolio/precache-manifest.9219c0e6de3b8973e9c04f7fce7734e9.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/landingpage/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
