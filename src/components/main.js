68a2a3cf088a53fd17901229e3ce921901d9b194
Eric Ablang <eablan2@uic.edu>
2020-08-26 00:46:28 -0500

    deleted opacity on css file

--- src/components/main.js
+++ src/components/main.js
@@ -8,7 +8,7 @@ import Resume from './resume';
 
 const Main = () => (
     <Switch>
-        <Route exact path="/landingpage" component = {LandingPage} />
+        <Route exact path="/" component = {LandingPage} />
         <Route path="/aboutme" component = {About} />
         <Route path="/contact" component = {Contact} />
         <Route path="/resume" component = {Resume} />
