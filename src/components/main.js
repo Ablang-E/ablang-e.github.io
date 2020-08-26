14d643f2fd69493750459fbf213aee9d260814f7
Eric Ablang <eablan2@uic.edu>
2020-08-26 00:25:51 -0500

    2nd commit

--- src/components/main.js
+++ src/components/main.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import LandingPage from './landingpage';
 import { Switch, Route } from 'react-router-dom';
+import LandingPage from './landingpage';
 import About from './aboutme';
 import Contact from './contact';
 import Projects from './projects';
@@ -8,7 +8,7 @@ import Resume from './resume';
 
 const Main = () => (
     <Switch>
-        <Route exact path="/landingpage" component = {LandingPage} />
+        <Route exact path="/" component = {LandingPage} />
         <Route path="/aboutme" component = {About} />
         <Route path="/contact" component = {Contact} />
         <Route path="/resume" component = {Resume} />
