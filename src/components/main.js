e01e8526e0e63c2b12111aaab3bae0ebd96272eb
Eric Ablang <eablan2@uic.edu>
2020-09-29 03:32:03 -0500

    Finalized fixes on react router issue

--- src/components/main.js
+++ src/components/main.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import LandingPage from './landingpage';
-import About from './aboutme';
+import About from './CS428_Projects';
 import Contact from './contact';
 import Projects from './projects';
 import Resume from './resume';
@@ -9,7 +9,7 @@ import Resume from './resume';
 const Main = () => (
     <Switch>
         <Route exact path="/" component = {LandingPage} />
-        <Route path="/aboutme" component = {About} />
+        <Route path="/CS428_Projects" component = {About} />
         <Route path="/contact" component = {Contact} />
         <Route path="/resume" component = {Resume} />
         <Route path="/projects" component = {Projects} />
